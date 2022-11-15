d9c56f1d6a44e109aea1a0d7b2ffabd8b14f9016
meriemkhattat <meriem.khattat@esprit.tn>
2022-11-15 23:54:59 +0100

    TestMeriem2

--- src/test1/java/tn/esprit/rh/achat/service/OperateurServiceImplMockTest.java
+++ src/test1/java/tn/esprit/rh/achat/service/OperateurServiceImplMockTest.java
@@ -18,7 +18,7 @@ import java.util.Optional;
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
 
-public class OperateurServiceImplMock {
+public class OperateurServiceImplMockTest {
 	@Mock
     OperateurRepository operateurRepository;
    
